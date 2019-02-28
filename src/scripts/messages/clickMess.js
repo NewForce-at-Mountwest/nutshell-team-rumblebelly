b4fec234ac08fcccae8eb78fa82dd5def6b769fc
Dale <dalesauljr@gmail.com>
2019-02-28 15:36:00 -0500

    fixed double print message err

--- src/scripts/messages/clickMess.js
+++ src/scripts/messages/clickMess.js
@@ -4,7 +4,7 @@ import printAllMess from "./printMess";
 
 const sendtheMess = () => {
 document.querySelector("#messageText").addEventListener("keyup", function(event){
-
+    // debugger;
     if (event.keyCode===13) {
         event.preventDefault();
         const messVal = document.querySelector("#messageText").value;
@@ -12,13 +12,13 @@ document.querySelector("#messageText").addEventListener("keyup", function(event)
 
         apiMess.postNewMess(messToPost)
         .then(() => {
-            apiMess.getAllMess();
+
+            (document.querySelector("#upperMess").innerHTML = "")
             printAllMess();
-
-
             document.querySelector("#messageText").value = "";
         })
     }
+
 })
 }
 
