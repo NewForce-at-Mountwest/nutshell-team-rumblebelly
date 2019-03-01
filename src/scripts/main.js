957bfb540b3ee87058d6e72716ac3e5df662bf82
MattRowee <29.matt@gmail.com>
2019-03-01 14:54:54 -0500

    CRUD

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,16 +1,20 @@
+//--@@@@@ EVENT IMPORTS
 import EVformBuilder from "./EVENTS/EVformBuilder"
 import APIManager from "./EVENTS/EVeventsAPIManager"
 import EventPrinter from "./EVENTS/eventList"
 import EVbuttonBuilder from "./EVENTS/EVbuttonBuilder"
+
+//--@@@@ MESSENGER IMPORTS
 import loadMessAPL from "./messages/printMess.js"
 import apiMess from "./messages/apiMess.js"
 import loadWelcome from "./messages/loadWelcome.js";
 import printAllMess from "./messages/printMess.js";
 
-
+//-- @@@ EVENT CALLS
 EVformBuilder.buildEventForm()
 APIManager.getAllEvents()
 EventPrinter()
 EVbuttonBuilder.activateEditButton()
-// EVbuttonBuilder.buildButton.activateEditButton()
+EVbuttonBuilder.activateDeleteButton()
+//--@@@@ MESSENGER CALLS
 printAllMess();
